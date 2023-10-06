4f7849eefdd3600f2ad623201a481089b133ec73
RanaNouman888 <nouman.arshad@accrualhub.com>
2023-10-06 10:49:15 +0500

    interview shedule

--- resources/views/ListYourJob.blade.php
+++ resources/views/ListYourJob.blade.php
@@ -4,14 +4,99 @@
 @endsection
 
 @section('content')
+<style>
+	.step {
+		display: none;
+	}
+
+	.active {
+		display: block;
+		color: rgba(0, 0, 0, 0.7) !important;
+	}
+
+	.dashboard-body .stepper {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		gap: 1%;
+	}
+
+	.dashboard-body #multi-step-form {
+		margin-top: 25px;
+	}
+
+	.dashboard-body .stepper>.step {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+		gap: 1rem;
+	}
+
+	.dashboard-body .stepper>.step>.icon>div {
+		border: 1px solid rgba(0, 0, 0, 0.5);
+		border-radius: 50%;
+		padding: 15%;
+		width: 30px;
+		height: 30px;
+	}
+
+	.dashboard-body .stepper>.step.selected>.icon>div {
+		background: #31795A;
+		color: #fff;
+	}
+
+	.dashboard-body .stepper>.step>.icon,
+	.dashboard-body .stepper>.step>.text {
+		font-size: 13px;
+		font-weight: 600;
+	}
+</style>
 <div class="dashboard-body">
-<div class="bg-white card-box border-20 section" id="step1">
-                   
-                   <h4 class="dash-title-three">Schedule Interview</h4>
+<div class="bg-white card-box border-20 section" >
+        <div class="stepper">
+            <div id="tag-step-1" class="step selected">
+                <div class="icon">
+                    <div>1</div>
+                </div>
+                <div class="text">Pending</div>
+            </div>
+            <div id="tag-step-2" class="step">
+                <div class="icon">
+                    <div>2</div>
+                </div>
+                <div class="text">Schedule Interview</div>
+            </div>
+            <div id="tag-step-3" class="step">
+                <div class="icon">
+                    <div>3</div>
+                </div>
+                <div class="text">Conditional Offer</div>
+            </div>
+            <div id="tag-step-4" class="step">
+                <div class="icon">
+                    <div>4</div>
+                </div>
+                <div class="text">Document & Final Offer</div>
+            </div>
+                            
+                            
+        </div>
+
+        <div class="py-4 step active" id="step1">
+            <div class="button-group d-inline-flex align-items-center mt-30" style="width:100%;justify-content: flex-end;">
+                <button type="button"  class="dash-btn-two tran3s" onclick="nextStep(1)">Next</button>
+            </div>
+        </div>
+
+        <div class="py-4 " id="step2">
+        <h4 class="dash-title-three">Schedule Interview</h4>
                    <form>
                         <div class="row">
                                             <div class="dash-input-wrapper mb-30 col-md-6">
-                                                <label for="">Start Date:</label>
+                                                <label for="">Interview Date:</label>
                                                 <input type="datetime-local" name="dateTime" placeholder="Select date and time" requird>
 
                                             </div>
@@ -22,7 +107,8 @@
                                             </div>
                         </div>
                         <div class="button-group d-inline-flex align-items-center mt-30" style="width:100%;justify-content: flex-end;">
-                                <button type="submit"  class="dash-btn-two tran3s">Submit</button>
+                        <button type="button" id="prevBtn" class="dash-cancel-btn tran3s  me-3" onclick="previousStep(2)">Previous</button>
+                                <button type="button"  class="dash-btn-two tran3s" onclick="nextStep(2)">Submit and Next</button>
                         </div>
                 </form>
                 <div>
@@ -31,24 +117,30 @@
                             <tr>
                                 <th>Sr</th>
                                 <th>Name</th>
+                                <th>Interview Date</th>
+                                <th>Status</th>
+
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td>Tiger Nixon</td>
+                                <td>1</td>
                                 <td>System Architect</td>
+                                <td>10/10/2023 2:05 pm</td>
+                                <td><button type="button" class="dash-btn-two tran3s" data-bs-toggle="modal" data-bs-target="#exampleModal">Acceptted</button></td>
                             </tr>
                             <tr>
-                                <td>Garrett Winters</td>
+                                <td>2</td>
                                 <td>Accountant</td>
+                                <td>10/10/2023 2:05 pm</td>
+                               <td><button type="button" class="dash-btn-two tran3s" data-bs-toggle="modal" data-bs-target="#exampleModal">Pending</button></td>
                             </tr>
                             
                         </tbody>
                 </table>
                 </div>
-</div>
-<div class="bg-white card-box border-20 section" id="step1">
-                   
+        </div>
+        <div class="py-4 step" id="step3">
                    <h4 class="dash-title-three">Conditional Offer</h4>
                    <form>
                         <div class="row">
@@ -100,12 +192,123 @@
                                 <textarea type="text" name="Additional benefits:" placeholder="" ></textarea>
 
                             </div>
+                            <div class="dash-input-wrapper mb-10 col-md-12">
+								<label style="font-size: 20px;font-weight: 600;">Conditions:</label>
+					        </div>
+                            <div class="dash-input-wrapper mb-30 col-md-6">
+                                        <label for="">Apostille Degree MUST be delivered by:</label>
+                                        <input type="date" name="endDate" placeholder=""></input>
+                            </div>
+                            <div class="dash-input-wrapper mb-30 col-md-6">
+                                        <label for="">Apostilled Criminal Clearance MUST be delivered by:</label>
+                                        <input type="date" name="endDate" placeholder=""></input>
+                            </div>
+                            <div class="dash-input-wrapper mb-30 col-md-6">
+                                        <label for="">SAQA Letter MUST be delivered by:</label>
+                                        <input type="date" name="endDate" placeholder=""></input>
+                            </div>
+                            <div class="dash-input-wrapper mb-30 col-md-6">
+                                        <label for="">Visa Documents and associated items things MUST be delivered by:</label>
+                                        <input type="date" name="endDate" placeholder=""></input>
+                            </div>
                             
 
                         </div>
                         <div class="button-group d-inline-flex align-items-center mt-30" style="width:100%;justify-content: flex-end;">
-                                <button type="submit"  class="dash-btn-two tran3s">Submit</button>
+                        <button type="button" id="prevBtn" class="dash-cancel-btn tran3s  me-3" onclick="previousStep(3)">Previous</button>
+                                <button type="button"  class="dash-btn-two tran3s" onclick="nextStep(3)">Submit and Next</button>
                         </div>
                 </form>
 </div>
+<div class="py-4 step" id="step4">
+        <h4 class="dash-title-three">Document & Final Offer</h4>
+                <div>
+                <table id="example" class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Sr</th>
+                                <th>Degree</th>
+                                <th>Status</th>
+                                <th>Deadline</th>
+                                <th>Link</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>System Architect</td>
+                                <td>Uploaded</td>
+                                <td>20/10/2023</td>
+                                <td>url</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Accountant</td>
+                                <td>Pending</td>
+                                <td>20/10/2023</td>
+                                <td>url</td>
+
+                            </tr>
+                            
+                        </tbody>
+                </table>
+                </div>
+                <div class="button-group d-inline-flex align-items-center mt-30" style="width:100%;justify-content: flex-end;">
+                        <button type="button" id="prevBtn" class="dash-cancel-btn tran3s  me-3" onclick="previousStep(4)">Previous</button>
+                                <button type="button"  class="dash-btn-two tran3s" onclick="nextStep(4)">Submit</button>
+                        </div>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title " id="exampleModalLabel" style="font-family:gordita">Ratting</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="dash-input-wrapper mb-30 col-md-6">
+                                    <label for=""> Specify:</label>
+                                    <select class="nice-select">
+                                        <option>KRW </option>
+                                        <option>USD</option>
+                                    </select>
+                        </div>
+                        <div class="dash-input-wrapper mb-30 col-md-6">
+                            <label for="">Comment:</label>
+                            <textarea type="text" name="Work and break hours" placeholder="Comment" ></textarea>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="dash-cancel-btn tran3s" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="dash-btn-two tran3s">Save changes</button>
+                </div>
+                </div>
+            </div>
+        </div>
+
+<script>
+	let currentStep = 1;
+
+	function nextStep(step) {
+		document.getElementById(`tag-step-${step}`).classList.remove('selected');
+        document.getElementById(`step${step}`).classList.remove('active');
+		currentStep =step+1;
+		document.getElementById(`step${currentStep}`).classList.add('active');
+		document.getElementById(`tag-step-${currentStep}`).classList.add('selected');
+	}
+
+	function previousStep(step) {
+        console.log("here");
+		document.getElementById(`step${step}`).classList.remove('active');
+		document.getElementById(`tag-step-${step}`).classList.remove('selected');
+		currentStep = step - 1;
+		document.getElementById(`step${currentStep}`).classList.add('active');
+		document.getElementById(`tag-step-${currentStep}`).classList.add('selected');
+	}
+</script>
 @endsection
