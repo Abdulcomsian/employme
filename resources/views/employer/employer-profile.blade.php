5cc03d703126d71752978edc7482dd7a73e32e85
shakir593 <faisalktk593@gmail.com>
2023-10-20 20:43:04 +0500

    ajax library changes

--- resources/views/employer/employer-profile.blade.php
+++ resources/views/employer/employer-profile.blade.php
@@ -983,7 +983,7 @@ Profile
 	</div>
 </div>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 
 <script>
 	let currentStep = 1;
