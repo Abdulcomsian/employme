fa2b1c67bce93004beedf1e582746ac45db78cd1
naccrual <nauman.riaz@accrualhub.com>
2024-04-22 16:11:52 +0500

    issue resolved

--- resources/views/company-about-us.blade.php
+++ resources/views/company-about-us.blade.php
@@ -106,17 +106,14 @@
 								<p>{!! $employerDetails->employer_details ?? '' !!}</p>
 								<h3>Intro</h3>
 							
-								@if($introductionVideo)
+								@if(!empty($employerDetails->introductry_video))
 								<div class="col-xxl-12 col-xl-12 order-xl-first card my-3 mr-1 p-1 padding-box">
-									<!-- <div class="video-post d-flex align-items-center justify-content-center mt-25 lg-mt-20 mb-50 lg-mb-50">
+									<div class="video-post d-flex align-items-center justify-content-center mt-25 lg-mt-20 mb-50 lg-mb-50">
 										<a class="fancybox rounded-circle video-icon tran3s text-center" data-fancybox=""
 											href="{{asset($employerDetails->introductry_video)}}">
 											<i class="bi bi-play"></i>
 										</a>
-									</div> -->
-									<video width="640" height="360" controls>
-										<source src="{{asset('uploads/employer/introduction-video/'.$introductionVideo->file_path)}}" type="video/mp4">
-									</video>
+									</div>
 								</div>
 								@endif
 								
@@ -221,19 +218,19 @@
 					</div>
 					
 					@if($introductionVideo)
-					<div class="inner-card mb-60 lg-mb-50">
-					<h3 class="title">Introduction</h3>
+					<!-- <div class="inner-card mb-60 lg-mb-50">
 						<div class="video-post d-flex align-items-center justify-content-center mt-25 lg-mt-20 mb-75 lg-mb-50">
 							<a class="fancybox rounded-circle video-icon tran3s text-center" data-fancybox="" href="http://127.0.0.1:8000/uploads/candidate/videos/171376439287871.mp4">
 								<i class="bi bi-play"></i>
 							</a>
 						</div>
-                    </div>
-						<!-- <div class="col-xxl-12 col-xl-12 order-xl-first card my-3 mr-1 p-1 padding-box">
+                    </div> -->
+					<div class="col-xxl-12 col-xl-12 order-xl-first card my-3 mr-1 p-1 padding-box">
+							<h3 class="title">Introduction</h3>
 							<video width="640" height="360" controls>
 								<source src="{{asset('uploads/employer/introduction-video/'.$introductionVideo->file_path)}}" type="video/mp4">
 							</video>
-						</div> -->
+						</div>
 					@endif
 
 					<div class="col-xxl-12 col-xl-12 order-xl-first card my-3 mr-1 p-1 padding-box">
