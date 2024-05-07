c5574801592ef05d8382413de2c28da0be9debf1
shakirfaisal <shakir.faisal@accrualhub.com>
2024-05-07 17:28:44 +0500

    changes in employer business operation module

--- resources/views/employer/business-operation/index.blade.php
+++ resources/views/employer/business-operation/index.blade.php
@@ -96,18 +96,26 @@ select.form-select {
                     </div>
                 </div>
                 @isset($businessOperationDetails->operation_time)
-                @foreach($businessOperationDetails->operation_time as $index=>$operationTime)
+                @foreach($businessOperationDetails->operation_time as $index => $operationTime)
                 @if($index == 0)
-                <div class="row" >
+                <div class="row time-field-row" >
                     <div class="col-lg-3">
                         <div class="dash-input-wrapper mb-20">
-                            <input type="text" name="operation_time[0][day]" value="{{$operationTime['day']}}" class = " @error('monday_start_time') is-invalid @enderror" placeholder="Day">
+                        <select name="operation_time[{{$index}}][day]" class="form-select">
+                                <option value="Monday" {{$operationTime['day'] == 'Monday' ? 'selected' : ''}}>Monday</option>
+                                <option value="Tuesday" {{$operationTime['day'] == 'Tuesday' ? 'selected' : ''}}>Tuesday</option>
+                                <option value="Wednesday" {{$operationTime['day'] == 'Wednesday' ? 'selected' : ''}}>Wednesday</option>
+                                <option value="Thursday" {{$operationTime['day'] == 'Thursday' ? 'selected' : ''}}>Thursday</option>
+                                <option value="Friday" {{$operationTime['day'] == 'Friday' ? 'selected' : ''}}>Friday</option>
+                                <option value="Saturday" {{$operationTime['day'] == 'Saturday' ? 'selected' : ''}}>Saturday</option>
+                                <option value="Sunday" {{$operationTime['day'] == 'Sunday' ? 'selected' : ''}}>Sunday</option>
+                            </select>
                         </div>
                         <!-- /.dash-input-wrapper -->
                     </div>
                     <div class="col-lg-3">
                         <div class="dash-input-wrapper mb-20">
-                            <input type="time" name="operation_time[0][start_time]" value="{{$operationTime['start_time']}}" class = " @error('monday_start_time') is-invalid @enderror" placeholder="Day">
+                            <input type="time" name="operation_time[{{$index}}][start_time]" value="{{$operationTime['start_time']}}" class = " @error('monday_start_time') is-invalid @enderror" placeholder="Day">
                             @error('monday_start_time')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -118,7 +126,7 @@ select.form-select {
                     </div>
                     <div class="col-lg-3">
                         <div class="dash-input-wrapper mb-20">
-                            <input type="time" name="operation_time[0][end_time]" value="{{$operationTime['end_time']}}" class = " @error('monday_end_time') is-invalid @enderror" placeholder="Day">
+                            <input type="time" name="operation_time[{{$index}}][end_time]" value="{{$operationTime['end_time']}}" class = " @error('monday_end_time') is-invalid @enderror" placeholder="Day">
                             @error('monday_end_time')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -133,28 +141,26 @@ select.form-select {
                         </div>
                     </div>
                 </div>
-                @endif
-                @endforeach
                 @else
-                <div class="row" >
+                <div class="row time-field-row" >
                     <div class="col-lg-3">
                         <div class="dash-input-wrapper mb-20">
-                            <select name="operation_time[$index][day]" class="form-select">
-                                <option value="Monday">Monday</option>
-                                <option value="Tuesday">Tuesday</option>
-                                <option value="Wednesday">Wednesday</option>
-                                <option value="Thursday">Thursday</option>
-                                <option value="Friday">Friday</option>
-                                <option value="Saturday">Saturday</option>
-                                <option value="Sunday">Sunday</option>
+                            <select name="operation_time[{{$index}}][day]" class="form-select">
+                                <option value="Monday" {{$operationTime['day'] == 'Monday' ? 'selected' : ''}}>Monday</option>
+                                <option value="Tuesday" {{$operationTime['day'] == 'Tuesday' ? 'selected' : ''}}>Tuesday</option>
+                                <option value="Wednesday" {{$operationTime['day'] == 'Wednesday' ? 'selected' : ''}}>Wednesday</option>
+                                <option value="Thursday" {{$operationTime['day'] == 'Thursday' ? 'selected' : ''}}>Thursday</option>
+                                <option value="Friday" {{$operationTime['day'] == 'Friday' ? 'selected' : ''}}>Friday</option>
+                                <option value="Saturday" {{$operationTime['day'] == 'Saturday' ? 'selected' : ''}}>Saturday</option>
+                                <option value="Sunday" {{$operationTime['day'] == 'Sunday' ? 'selected' : ''}}>Sunday</option>
                             </select>
-                            {{-- <input type="text" name="operation_time[$index][day]" value="" class = " @error('monday_start_time') is-invalid @enderror" placeholder="Day"> --}}
+                            {{-- <input type="text" name="operation_time[$index][day]" value="{{$operationTime['start_time']}}" class = " @error('monday_start_time') is-invalid @enderror" placeholder="Day"> --}}
                         </div>
                         <!-- /.dash-input-wrapper -->
                     </div>
                     <div class="col-lg-3">
                         <div class="dash-input-wrapper mb-20">
-                            <input type="time" name="operation_time[$index][start_time]" value="{{$businessOperationDetails->monday_start_time ?? ''}}" class = " @error('monday_start_time') is-invalid @enderror" placeholder="Day">
+                            <input type="time" name="operation_time[{{$index}}][start_time]" value="{{$operationTime['start_time']}}" class = " @error('monday_start_time') is-invalid @enderror" placeholder="Day">
                             @error('monday_start_time')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -165,7 +171,52 @@ select.form-select {
                     </div>
                     <div class="col-lg-3">
                         <div class="dash-input-wrapper mb-20">
-                            <input type="time" name="operation_time[$index][end_time]" value="{{$businessOperationDetails->monday_end_time ?? ''}}" class = " @error('monday_end_time') is-invalid @enderror" placeholder="Day">
+                            <input type="time" name="operation_time[{{$index}}][end_time]" value="{{$operationTime['end_time']}}" class = " @error('monday_end_time') is-invalid @enderror" placeholder="Day">
+                            @error('monday_end_time')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+                        <!-- /.dash-input-wrapper -->
+                    </div>
+                    <div class="col-lg-3">
+                            <div class="dash-input-wrapper mb-20">
+                                <button type="button" class="btn btn-danger remove-tr">Remove</button>
+                            </div>
+                    </div>
+                </div>
+                @endif
+                @endforeach
+                @else
+                <div class="row time-field-row" >
+                    <div class="col-lg-3">
+                        <div class="dash-input-wrapper mb-20">
+                        <select name="operation_time[0][day]" class="form-select">
+                                <option value="Monday">Monday</option>
+                                <option value="Tuesday">Tuesday</option>
+                                <option value="Wednesday">Wednesday</option>
+                                <option value="Thursday">Thursday</option>
+                                <option value="Friday">Friday</option>
+                                <option value="Saturday">Saturday</option>
+                                <option value="Sunday">Sunday</option>
+                            </select>                        </div>
+                        <!-- /.dash-input-wrapper -->
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="dash-input-wrapper mb-20">
+                            <input type="time" name="operation_time[0][start_time]" value="" class = " @error('monday_start_time') is-invalid @enderror" placeholder="Day">
+                            @error('monday_start_time')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+                        <!-- /.dash-input-wrapper -->
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="dash-input-wrapper mb-20">
+                            <input type="time" name="operation_time[0][end_time]" value="" class = " @error('monday_end_time') is-invalid @enderror" placeholder="Day">
                             @error('monday_end_time')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -182,34 +233,7 @@ select.form-select {
                 </div>
                 @endisset
                     <div id="add-time-field">
-                    @isset($businessOperationDetails->operation_time)
-                    @foreach($businessOperationDetails->operation_time as $index=>$operationTime)
-                    @if($index != 0)
-                    <div class="row time-field-row">
-                        <div class="col-lg-3">
-                            <div class="dash-input-wrapper mb-20">
-                                <input type="text" name="operation_time[$index][day]"  placeholder="Day" value = "{{$operationTime['day']}}">
-                            </div>
-                        </div>
-                        <div class="col-lg-3">
-                            <div class="dash-input-wrapper mb-20">
-                                <input type="time" name="operation_time[$index][start_time]"  placeholder="Add Skill" value = "{{$operationTime['start_time']}}">
-                            </div>
-                        </div>
-                        <div class="col-lg-3">
-                            <div class="dash-input-wrapper mb-20">
-                                <input type="time" name="operation_time[$index][end_time]"  placeholder="Add Skill" value = "{{$operationTime['end_time']}}">
-                            </div>
-                        </div>
-                        <div class="col-lg-3">
-                            <div class="dash-input-wrapper mb-20">
-                                <button type="button" class="btn btn-danger remove-tr">Remove</button>
-                            </div>
-                        </div>
-                    </div>
-                    @endif
-                    @endforeach
-                    @endisset
+                   
                     </div>
                
              
@@ -236,12 +260,9 @@ $('#summernote').summernote({
         height: 200
  });
 
- var i = 0;
+ var i = $(".time-field-row").length;
        
        $("#add").click(function(){
-      
-           ++i;
-      
         //    $("#add-skill-field").append('<tr><td><input type="text" name="addmore['+i+'][name]" placeholder="Enter your Name" class="form-control" /></td><td><input type="text" name="addmore['+i+'][qty]" placeholder="Enter your Qty" class="form-control" /></td><td><input type="text" name="addmore['+i+'][price]" placeholder="Enter your Price" class="form-control" /></td><td><button type="button" class="btn btn-danger remove-tr">Remove</button></td></tr>');
            $("#add-time-field").append('<div class="row time-field-row"><div class="col-lg-3"><div class="dash-input-wrapper mb-20"><select class="form-select"  name="operation_time['+i+'][day]" ><option value="Monday">Monday</option><option value="Tuesday">Tuesday</option><option value="Wednesday">Wednesday</option><option value="Thursday">Thursday</option><option value="Friday">Friday</option><option value="Saturday">Saturday</option><option value="Sunday">Sunday</option></select></div></div><div class="col-lg-3"><div class="dash-input-wrapper mb-20"><input type="time" name="operation_time['+i+'][start_time]"  placeholder="Add Skill" value = ""></div></div><div class="col-lg-3"><div class="dash-input-wrapper mb-20"><input type="time" name="operation_time['+i+'][end_time]"  placeholder="Add Skill" value = ""></div></div><div class="col-lg-3"><div class="dash-input-wrapper mb-20"><button type="button" class="btn btn-danger remove-tr">Remove</button></div></div></div>')
        });
