8ebd91eec8ade2105bdd964abbaad414580410bd
Abdul Basit <basitawan.abdul@gmail.com>
2024-06-06 19:08:45 +0500

    updated code

--- resources/views/employer/jobs/create.blade.php
+++ resources/views/employer/jobs/create.blade.php
@@ -181,7 +181,7 @@ Post A Job
                         </div>
                         <div class="dash-input-wrapper mb-30 col-md-6">
                             <label for="">Monthly Pay:</label>
-                            <input type="text" pattern="^[0-9.-]" list="monthly_amount" name="monthly_salary"/>
+                            <input type="text" onkeypress="return isNumberKey(event)" list="monthly_amount" name="monthly_salary"/>
                             {{-- <datalist id="monthly_amount">
                                 <option>Negotiation</option>
                                 <option>2.5k - 3k</option>
@@ -931,5 +931,12 @@ Post A Job
        
     });
 </script> -->
+function isNumberKey(evt)
+{
+   var charCode = (evt.which) ? evt.which : event.keyCode
+   if (charCode != 45  && charCode > 31 && (charCode < 48 || charCode > 57))
+      return false;
 
+   return true;
+}
     @endsection
