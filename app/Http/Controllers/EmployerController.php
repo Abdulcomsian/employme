980351f8a73fa341a96bae3007859fac9a3c8fe9
naccrual <nauman.riaz@accrualhub.com>
2024-07-11 17:50:56 +0500

    issue tsting

--- app/Http/Controllers/EmployerController.php
+++ app/Http/Controllers/EmployerController.php
@@ -366,8 +366,8 @@ class EmployerController extends Controller
                                                 })
                                                 ->where('employer_job_id' , $jobId)
                                                 ->count();
+        dd($existingInterviewRequest);
 
-dd($existingInterviewRequest );
         if($existingInterviewRequest === 0 ){
 
             $candidateDetails = User::with('candidatePersonalDetails')->find($request->candidate_id);
